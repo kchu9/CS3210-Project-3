f02df736f8427a634546a3bc64e3719f8da96ac4
kchu9 <kchu9@gatech.edu>
2013-03-11 21:02:36 -0400

    commit

--- sysInterposter.c
+++ sysInterposter.c
@@ -5,8 +5,10 @@
 MODULE_LICENSE("GPL");
  
 #define MODULE_NAME "[sysmon] "
-static struct kprobe probe;
-static uid_t uid; 
+static int numElements=2;
+static struct kprobe probe[2];
+static uid_t uid;
+static char *sysCalls[]={"sys_mkdir","sys_rmdir"}; 
 /* pt_regs defined in include/asm-x86/ptrace.h
  *
  * For information associating registers with function arguments, see:
@@ -16,17 +18,26 @@ static int sysmon_intercept_before(struct kprobe *kp, struct pt_regs *regs)
 {
 
     int ret = 0;
+    long val=regs->rax;
     if (current->uid != uid)
-        return 0;
-    switch (regs->rax) {
+        return 0;	
+    switch (val) {
         case __NR_mkdir:
             printk(KERN_INFO MODULE_NAME
                     /* sycall pid tid args.. */
-                    "%lu %d %d args 0x%lu '%s' %d\n",
-                    regs->rax, current->pid, current->tgid,
+                    "mkdir call: %lu %d %d args 0x%lu '%s' %d\n",
+                    val, current->pid, current->tgid,
                     (uintptr_t)regs->rdi, (char*)regs->rdi, (int)regs->rsi);
             break;
-        default:
+        case __NR_rmdir:
+            printk(KERN_INFO MODULE_NAME
+                    "rmdir call: %lu %d %d args\n",
+                    regs->rax, current->pid, current->tgid);
+            break;
+	case __NR_access:
+	    printk(KERN_INFO MODULE_NAME "access call:  args 0x");
+	    break;
+	default:
             ret = 100;
             break;
     }
@@ -41,21 +52,31 @@ static void sysmon_intercept_after(struct kprobe *kp, struct pt_regs *regs,
 }
  
 int init_module(void)
-{	
+{
+    int i;	
     uid=282853;
-    probe.symbol_name = "sys_mkdir";
-    probe.pre_handler = sysmon_intercept_before; /* called prior to function */
-    probe.post_handler = sysmon_intercept_after; /* called on function return */
-    if (register_kprobe(&probe)) {
-        printk(KERN_ERR MODULE_NAME "register_kprobe failed\n");
-        return -EFAULT;
-    }
+	printk(KERN_INFO MODULE_NAME "%s",sysCalls[0]);
+   /* probe.symbol_name = "sys_mkdir"; */   
+    probe[0].symbol_name = sysCalls[0];
+    probe[0].pre_handler = sysmon_intercept_before; /* called prior to function */
+    probe[0].post_handler = sysmon_intercept_after; /* called on function return */
+	for(i=0;i<numElements;i++)
+	{
+ 	 if (register_kprobe(&probe[i])) {
+       	 printk(KERN_ERR MODULE_NAME "register_kprobe failed\n");
+       	 return -EFAULT;
+    	}
+	}
     printk(KERN_INFO MODULE_NAME "loaded\n");
     return 0;
 }
  
 void cleanup_module(void)
 {
-    unregister_kprobe(&probe);
+    int i=0;
+   for(i=0;i<numElements;i++)
+  {
+    unregister_kprobe(&probe[i]);
     printk(KERN_INFO MODULE_NAME "unloaded\n");
+	}
 }
